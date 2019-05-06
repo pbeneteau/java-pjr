63721d67b8c6bbc1f8fe37763c54776524f8f8c3
BÉNÉTEAU Paul <paul.beneteau@efrei.net>
2019-05-06 08:33:37 +0200

    fef

--- src/Promotion.java
+++ src/Promotion.java
@@ -35,26 +35,19 @@ private String nom;
     public void setNom(String nom) {
         this.nom = nom;
     }
-    public Eleve rechercher(int numeroIdentifiant){
-        boolean exist=false;
+
+    public Eleve searchStudent(int studentNumber){
 
         if(this.listEleves.isEmpty()) {
             return null;
         }
 
-        for(int i=0;i<this.listEleves.size();i++){
-            if(this.listEleves.get(i).getNumeroIdentifiant() == numeroIdentifiant){
-                exist=true;
-                break;
+        for (Eleve listEleve : this.listEleves) {
+
+            if (listEleve.getNumeroIdentifiant() == studentNumber) {
+                return listEleve;
             }
         }
-
-        if(exist) {
-            return this.listEleves.get(numeroIdentifiant);
-        } else {
-            return this.listEleves.get(numeroIdentifiant);
-        }
-
-
+        return null;
     }
 }
